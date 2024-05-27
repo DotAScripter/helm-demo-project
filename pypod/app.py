3b37a97a9a4dcc1183eb1615574b79650827167a
BuildTools <unconfigured@null.spigotmc.org>
2024-05-27 18:29:09 +0000

    Add status service for pypod

--- pypod/app.py
+++ pypod/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from concurrent import futures
 import os
 import logging
 import sys
+import grpc
+import threading
+from proto.status import status_pb2
+from proto.status import status_pb2_grpc
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
@@ -12,8 +17,24 @@ def index():
     logging.info("Got request on /")
     return "Hello world!"
 
+class StatusServicer(status_pb2_grpc.StatusServicer):
+    def CheckStatus(self, request, context):
+        logging.info("Server listening on port:%s", request.service_name)
+        return status_pb2.StatusCheckResponse(status=status_pb2.StatusCheckResponse.OK)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    status_pb2_grpc.add_StatusServicer_to_server(StatusServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    logging.info("Server started on port:50051")
+    server.wait_for_termination()
+
 if __name__ == "__main__":
+    gprc_thread = threading.Thread(target=serve)
+    gprc_thread.start()
     from waitress import serve
     port = os.environ.get('HTTP_PORT', '3000')
     logging.info("Server listening on port:%s", port)
     serve(app, host="0.0.0.0", port=port)
+    gprc_thread.join()
