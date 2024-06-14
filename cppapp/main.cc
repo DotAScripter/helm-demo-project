69c1476240c9656a1e23ae21999fdbab50c10c78
Simon A Andersson <simon.a.andersson@tietoevry.com>
2024-06-14 12:49:35 +0200

    Fix formatting C++ app, update main.cc

--- cppapp/main.cc
+++ cppapp/main.cc
@@ -13,8 +13,8 @@ using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
 using helloworld::Greeter;
-using helloworld::HelloRequest;
 using helloworld::HelloReply;
+using helloworld::HelloRequest;
 
 std::mutex mutex;
 bool shutdownRequired = false;
@@ -22,34 +22,43 @@ std::condition_variable cv;
 std::unique_ptr<Server> greeterServer;
 std::unique_ptr<Server> statusServer;
 
-std::string getEnvOrDefault(const char* name, const std::string& defaultValue) {
-    const char* envValue = std::getenv(name);
-    if (envValue == nullptr) {
+std::string getEnvOrDefault(const char *name, const std::string &defaultValue)
+{
+    const char *envValue = std::getenv(name);
+    if (envValue == nullptr)
+    {
         return defaultValue;
     }
     return std::string(envValue);
 }
 
-void handleSignal(int sig) {
+void handleSignal(int sig)
+{
     std::cout << "Received signal: " << strsignal(sig) << std::endl;
     shutdownRequired = true;
     cv.notify_one();
 }
 
-void shutdownCheckingThread(void){
+void shutdownCheckingThread(void)
+{
     std::unique_lock<std::mutex> lock(mutex);
-    cv.wait(lock, [&](){ return shutdownRequired; });
-    if (greeterServer != nullptr) {
+    cv.wait(lock, [&]()
+            { return shutdownRequired; });
+    if (greeterServer != nullptr)
+    {
         greeterServer->Shutdown();
     }
-    if (statusServer != nullptr) {
+    if (statusServer != nullptr)
+    {
         statusServer->Shutdown();
     }
 }
 
-class GreeterServiceImpl final : public Greeter::Service {
-    Status SayHello(ServerContext* context, const HelloRequest* request,
-                    HelloReply* reply) override {
+class GreeterServiceImpl final : public Greeter::Service
+{
+    Status SayHello(ServerContext *context, const HelloRequest *request,
+                    HelloReply *reply) override
+    {
         std::cout << "GreeterServiceImpl handling HelloRequest: " << request->DebugString() << std::endl;
         std::string message = "Hello, " + request->name();
         reply->set_message(message);
@@ -57,14 +66,17 @@ class GreeterServiceImpl final : public Greeter::Service {
     }
 };
 
-class StatusServiceImpl final : public status::Status::Service {
-  grpc::Status CheckStatus(ServerContext* context, const status::StatusCheckRequest* request, status::StatusCheckResponse* response) override {
-     std::cout << "StatusServiceImpl handling StatusCheckRequest: " << request->DebugString() << std::endl;
-    return grpc::Status::OK;
-  }
+class StatusServiceImpl final : public status::Status::Service
+{
+    grpc::Status CheckStatus(ServerContext *context, const status::StatusCheckRequest *request, status::StatusCheckResponse *response) override
+    {
+        std::cout << "StatusServiceImpl handling StatusCheckRequest: " << request->DebugString() << std::endl;
+        return grpc::Status::OK;
+    }
 };
 
-void runStatusServer() {
+void runStatusServer()
+{
     const std::string serverPort = getEnvOrDefault("STATUS_SERVICE_PORT", "50052");
     const std::string serverAddress = getEnvOrDefault("STATUS_SERVICE_ADDRESS", "127.0.0.1");
     const std::string serverAddressWithPort = serverAddress + ":" + serverPort;
@@ -81,7 +93,8 @@ void runStatusServer() {
     statusServer->Wait();
 }
 
-void runGreeterServer() {
+void runGreeterServer()
+{
     const std::string serverPort = getEnvOrDefault("GREETER_SERVICE_PORT", "50051");
     const std::string serverAddress = getEnvOrDefault("GREETER_SERVICE_ADDRESS", "127.0.0.1");
     const std::string serverAddressWithPort = serverAddress + ":" + serverPort;
@@ -98,19 +111,22 @@ void runGreeterServer() {
     greeterServer->Wait();
 }
 
-int main() {
-    std::signal(SIGTERM, handleSignal);
-    std::thread greeterServerThread(runGreeterServer);
-    std::thread statusServerThread(runStatusServer);
-    std::thread shutdownThread(shutdownCheckingThread);
-    int exitCode = 0;
-    try {
+int main()
+{
+    try
+    {
+        std::signal(SIGTERM, handleSignal);
+        std::thread greeterServerThread(runGreeterServer);
+        std::thread statusServerThread(runStatusServer);
+        std::thread shutdownThread(shutdownCheckingThread);
         greeterServerThread.join();
         statusServerThread.join();
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        exitCode = 1;
+        shutdownThread.join();
+        return 0;
     }
-    shutdownThread.join();
-    std::exit(exitCode);
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
 }
